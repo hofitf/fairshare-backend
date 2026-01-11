8795e26acc1e42c57cb7700fa4f9471c5e1e3f36
hofitf <hofit9@gmail.com>
2026-01-11 12:17:34 +0200

    Add group CRUD endpoints and user creation

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine, Session
 from models import User, Group
 
 sqlite_file_name = "fairshare.db"
@@ -9,6 +9,12 @@ engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:  # with will run session.close() when needed
+        yield session
+
 
 if __name__ == "__main__":
     create_db_and_tables()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-from database import create_db_and_tables
+from fastapi import FastAPI, Depends
+from sqlmodel import Session, select
+from database import create_db_and_tables, get_session
+from models import User, Group
 
 from contextlib import asynccontextmanager
 
@@ -15,4 +17,29 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to FairShare API!", "Status": "active"}
+
+
+@app.post("/users/")
+def create_user(user: User, session: Session = Depends(get_session)):
+    session.add(user)  # adding user to session (no ID yet)
+    session.commit()  # sending sql query to SQLite to add user, DB  assigns ID
+    session.refresh(user)  # sending current data from DB to python. SELECT query
+    return user
+
+
+@app.post("/groups/")
+def create_group(group: Group, session: Session = Depends(get_session)):
+    session.add(group)
+    session.commit()
+    session.refresh(group)
+    return group
+
+@app.get("/groups/")
+def read_groups(session: Session = Depends(get_session)):
+    statement = select(Group)
+    results = session.exec(statement)
+    groups = results.all()
+    return groups
+
+
 
